e67631f184bd2a1b967a8f3b373bd7770e41e322
julesbe <jules.bertrand@epitech.eu>
2024-05-11 15:45:40 -0700

    tetete

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
-class Config:
-    SECRET_KEY = 'Abdoul'  # Better to use an environment variable here
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = "973b8d7253c024d2cb6d0055c1472935d74d8fcfbe5e9620ed31c267da038b25"
+SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+PERMANENT_SESSION_LIFETIME = 86400
+ALLOWED_EXTENSIONS = {'.py'}
 
-class DevelopmentConfig(Config):
+class DevelopmentConfig:
     DEBUG = True
 
-class ProductionConfig(Config):
+class ProductionConfig:
     DEBUG = False
 
 class CeleryConfig:
